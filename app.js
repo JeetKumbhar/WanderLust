847b5a9940fe131add88703933e138ae185f6fd4
Jeet Kumbhar <163125983+JeetKumbhar@users.noreply.github.com>
2025-12-28 11:45:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ async function main() {
 }
 
 
-app.set("veiw engine", "ejs");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
